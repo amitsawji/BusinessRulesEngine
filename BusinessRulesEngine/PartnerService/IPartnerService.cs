ae3d574db4fc279c419b5824f2c00156509fcbac
amitsawji <sawji.amit@gmail.com>
2020-09-12 18:31:42 +0530

    Adde partner services to gracefully handle third party services

---
+++ BusinessRulesEngine/PartnerService/IPartnerService.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessRulesEngine.ThirdPartyOperations
+{
+    /// <summary>
+    /// Interface for handling thirt party operations. To keep this short,
+    /// all methods are added in a single interface. 
+    /// </summary>
+    public interface IPartnerService
+    {
+        void GeneratePackingSlip();
+        void ActivateMembership();
+        void UpgradeMembership();
+        void SendEmail();
+        void GenerateCommision();
+        void MakePayment();
+    }
+}
---
+++ BusinessRulesEngine/PartnerService/PartnerService.cs
@@ -0,0 +1,40 @@
+﻿using BusinessRulesEngine.ThirdPartyOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRulesEngine.PartnerService
+{
+    public class PartnerService : IPartnerService
+    {
+        public void ActivateMembership()
+        {
+            Console.WriteLine("Membership activated!");
+        }
+
+        public void GenerateCommision()
+        {
+            Console.WriteLine("Commision Generated!");
+        }
+
+        public void GeneratePackingSlip()
+        {
+            Console.WriteLine("Packing slip generated!");
+        }
+
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment successful!");
+        }
+
+        public void SendEmail()
+        {
+            Console.WriteLine("Email sent!");
+        }
+
+        public void UpgradeMembership()
+        {
+            Console.WriteLine("Membership upgraded!");
+        }
+    }
+}
