3e803174833f5f0c87309651d21f3e58b34e6d34
amitsawji <sawji.amit@gmail.com>
2020-09-12 18:04:47 +0530

    Added logic to Book business rules and unit tests

--- BusinessRulesEngine/BusinessRules/BookBusinessRule.cs
+++ BusinessRulesEngine/BusinessRules/BookBusinessRule.cs
@@ -7,10 +7,23 @@ namespace BusinessRulesEngine.BusinessRules
     public class BookBusinessRule : IProductBusinessRule
     {
         public ProductType ProductType => ProductType.Book;
+        int gstPercentage = 0;
+        int commistionPercentage = 5;
 
         public void Execute(IProduct product)
         {
-            
+            if (product == null) throw new ArgumentNullException();
+
+            //There is a better way to do this by injecting the services which facilitate packing slip generation
+            //and payment. However it is out of scope for this assessment.
+
+            product.FinalPrice = product.Price + (product.Price * gstPercentage) / 100;
+
+            Console.WriteLine($"Duplicate packing Slip generated royalty department for product -{product.ProductName}");
+
+            var commision = (product.Price * commistionPercentage) / 100;
+
+            Console.WriteLine($"Payment of {commision} was sent to Agent");
         }
     }
 }
--- BusinessRulesEngineTests/BookBusinessRulesTest.cs
+++ BusinessRulesEngineTests/BookBusinessRulesTest.cs
@@ -1,19 +1,20 @@
-using AutoFixture;
+﻿using AutoFixture;
 using BusinessRulesEngine.BusinessRules;
 using BusinessRulesEngine.Models;
 using FluentAssertions;
 using NUnit.Framework;
 using System;
 
+
 namespace BusinessRulesEngineTests
 {
-    public class PhysicalProductRuleTests : TestBase
+    public class BookBusinessRulesTest : TestBase
     {
         [Test]
         public void Given_A_Product_Is_Null_When_Execute_Is_Called_Throws_ArgumentNullException()
         {
             //arrange 
-            var sut = new PhysicalProductRule();
+            var sut = new BookBusinessRule();
 
             // Act
             Action act = () => sut.Execute(null);
@@ -26,15 +27,15 @@ namespace BusinessRulesEngineTests
         public void Given_A_Product_When_Execute_Is_Called_Adds_GST_To_Final_Price()
         {
             //arrange 
-            var product = _fixture.Create<PhysicalProduct>();
-            product.Price = 100;
-            var sut = new PhysicalProductRule();
+            var product = _fixture.Create<Book>();
+            product.Price = 50;
+            var sut = new BookBusinessRule();
 
             // Act
             sut.Execute(product);
 
             //Assert
-            product.FinalPrice.Should().Equals(120);
+            product.FinalPrice.Should().Equals(56);
         }
     }
 }
